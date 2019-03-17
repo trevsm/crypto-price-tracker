8a35ccd6917c91de4b5fc8472a08b7a01b37cbfe
unknown <16tsmith55@gmail.com>
2019-03-17 11:32:44 -0600

    Fully working unstyled, but working tick

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,43 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import "../styles/App.scss";
 
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      listings: []
+    };
+    this.tick = this.tick.bind(this);
   }
   componentDidMount() {
-    console.log("Mounted Successfully");
+    window.setInterval(this.tick, 1000);
+    console.log("loading");
+  }
+  tick() {
+    console.log("tick");
+    axios
+      .get(
+        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD"
+      )
+      .then(res => {
+        const raw = res.data;
+        this.setState({
+          listings: raw
+        });
+      });
   }
   render() {
+    const { listings } = this.state;
     return (
-      <div>
-        <h1>React!</h1>
-      </div>
+      <>
+        {Object.keys(listings).map(key => (
+          <div className="coin" key={key}>
+            <div>{key}</div>
+            <div>{listings[key].USD} USD</div>
+          </div>
+        ))}
+      </>
     );
   }
 }
