61c05c3d963fb989b465ac1696f166b4b9b76deb
unknown <16tsmith55@gmail.com>
2019-03-17 23:22:23 -0600

    Rewrote for correct use of this.state

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listings: []
+      listings: {
+        BTC: { USD: 0 },
+        ETH: { USD: 0 },
+        LTC: { USD: 0 },
+        XRP: { USD: 0 },
+        XLM: { USD: 0 }
+      }
     };
+
     this.tick = this.tick.bind(this);
   }
   componentDidMount() {
+    this.tick();
     window.setInterval(this.tick, 1000);
-    console.log("loading");
   }
   tick() {
-    console.log("tick");
-    axios
-      .get(
-        "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=USD"
-      )
-      .then(res => {
-        const raw = res.data;
+    const { listings } = this.state;
+    const URL = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${Object.keys(
+      listings
+    )}&tsyms=USD`;
+
+    fetch(URL)
+      .then(raw => raw.json())
+      .then(raw => {
         this.setState({
           listings: raw
         });
@@ -31,12 +39,18 @@ class App extends Component {
     const { listings } = this.state;
     return (
       <>
-        {Object.keys(listings).map(key => (
-          <div className="coin" key={key}>
-            <div>{key}</div>
-            <div>{listings[key].USD} USD</div>
-          </div>
-        ))}
+        <div key={Math.random()}>
+          {Object.keys(listings).map(e => (
+            <div key={Math.random()}>{e}</div>
+          ))}
+          {Object.values(listings).map(e =>
+            Object.values(e).map(price => (
+              <div key={Math.random()} style={{ color: "blue" }}>
+                {price}
+              </div>
+            ))
+          )}
+        </div>
       </>
     );
   }
