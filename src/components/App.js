89bc32b23345f486c5eb7f9d75678456eba6306c
unknown <16tsmith55@gmail.com>
2019-03-21 01:20:40 -0600

    fixed animation cut off bug

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import "@babel/polyfill";
 import Contact from "./Contact";
 import "../styles/App.scss";
 
+//not paid free api key -- max of 50 calls per second
 const API_KEY = "";
 
 class App extends Component {
@@ -11,87 +12,87 @@ class App extends Component {
     super(props);
     this.state = {
       listings: {
-        BTC: { USD: 0 },
-        ETH: { USD: 0 },
-        XRP: { USD: 0 },
-        LTC: { USD: 0 },
-        EOS: { USD: 0 },
-        BCH: { USD: 0 },
-        BNB: { USD: 0 },
-        XLM: { USD: 0 },
-        USDT: { USD: 0 },
-        TRX: { USD: 0 },
-        ADA: { USD: 0 },
-        BSV: { USD: 0 },
-        XMR: { USD: 0 },
-        DASH: { USD: 0 },
-        MKR: { USD: 0 },
-        NEO: { USD: 0 },
-        ONT: { USD: 0 },
-        ETC: { USD: 0 },
-        ZEC: { USD: 0 },
-        XTZ: { USD: 0 },
-        VET: { USD: 0 },
-        WAVES: { USD: 0 },
-        DOGE: { USD: 0 },
-        USDC: { USD: 0 },
-        BAT: { USD: 0 },
-        BTG: { USD: 0 },
-        QTUM: { USD: 0 },
-        OMG: { USD: 0 },
-        DCR: { USD: 0 },
-        LSK: { USD: 0 },
-        LINK: { USD: 0 },
-        DGB: { USD: 0 },
-        REP: { USD: 0 },
-        ZIL: { USD: 0 },
-        ABBC: { USD: 0 },
-        ICX: { USD: 0 },
-        ZRX: { USD: 0 },
-        HOT: { USD: 0 },
-        BCN: { USD: 0 },
-        BTT: { USD: 0 },
-        STEEM: { USD: 0 },
-        NANO: { USD: 0 },
-        BTS: { USD: 0 },
-        BCD: { USD: 0 },
-        ENJ: { USD: 0 },
-        THETA: { USD: 0 },
-        KMD: { USD: 0 },
-        PAX: { USD: 0 },
-        AE: { USD: 0 },
-        NPXS: { USD: 0 },
-        XVG: { USD: 0 },
-        SC: { USD: 0 },
-        BTM: { USD: 0 },
-        HT: { USD: 0 },
-        RVN: { USD: 0 },
-        DAI: { USD: 0 },
-        STRAT: { USD: 0 },
-        AOA: { USD: 0 },
-        IOST: { USD: 0 },
-        SNT: { USD: 0 },
-        PPT: { USD: 0 },
-        KCS: { USD: 0 },
-        GNT: { USD: 0 },
-        REPO: { USD: 0 },
-        PAI: { USD: 0 },
-        ARK: { USD: 0 },
-        ARDR: { USD: 0 },
-        R: { USD: 0 }
+        BTC: { USD: 0, changed: 0, indication: 0 },
+        ETH: { USD: 0, changed: 0, indication: 0 },
+        XRP: { USD: 0, changed: 0, indication: 0 },
+        LTC: { USD: 0, changed: 0, indication: 0 },
+        EOS: { USD: 0, changed: 0, indication: 0 },
+        BCH: { USD: 0, changed: 0, indication: 0 },
+        BNB: { USD: 0, changed: 0, indication: 0 },
+        XLM: { USD: 0, changed: 0, indication: 0 },
+        USDT: { USD: 0, changed: 0, indication: 0 },
+        TRX: { USD: 0, changed: 0, indication: 0 },
+        ADA: { USD: 0, changed: 0, indication: 0 },
+        BSV: { USD: 0, changed: 0, indication: 0 },
+        XMR: { USD: 0, changed: 0, indication: 0 },
+        DASH: { USD: 0, changed: 0, indication: 0 },
+        MKR: { USD: 0, changed: 0, indication: 0 },
+        NEO: { USD: 0, changed: 0, indication: 0 },
+        ONT: { USD: 0, changed: 0, indication: 0 },
+        ETC: { USD: 0, changed: 0, indication: 0 },
+        ZEC: { USD: 0, changed: 0, indication: 0 },
+        XTZ: { USD: 0, changed: 0, indication: 0 },
+        VET: { USD: 0, changed: 0, indication: 0 },
+        WAVES: { USD: 0, changed: 0, indication: 0 },
+        DOGE: { USD: 0, changed: 0, indication: 0 },
+        USDC: { USD: 0, changed: 0, indication: 0 },
+        BAT: { USD: 0, changed: 0, indication: 0 },
+        BTG: { USD: 0, changed: 0, indication: 0 },
+        QTUM: { USD: 0, changed: 0, indication: 0 },
+        OMG: { USD: 0, changed: 0, indication: 0 },
+        DCR: { USD: 0, changed: 0, indication: 0 },
+        LSK: { USD: 0, changed: 0, indication: 0 },
+        LINK: { USD: 0, changed: 0, indication: 0 },
+        DGB: { USD: 0, changed: 0, indication: 0 },
+        REP: { USD: 0, changed: 0, indication: 0 },
+        ZIL: { USD: 0, changed: 0, indication: 0 },
+        ABBC: { USD: 0, changed: 0, indication: 0 },
+        ICX: { USD: 0, changed: 0, indication: 0 },
+        ZRX: { USD: 0, changed: 0, indication: 0 },
+        HOT: { USD: 0, changed: 0, indication: 0 },
+        BCN: { USD: 0, changed: 0, indication: 0 },
+        BTT: { USD: 0, changed: 0, indication: 0 },
+        STEEM: { USD: 0, changed: 0, indication: 0 },
+        NANO: { USD: 0, changed: 0, indication: 0 },
+        BTS: { USD: 0, changed: 0, indication: 0 },
+        BCD: { USD: 0, changed: 0, indication: 0 },
+        ENJ: { USD: 0, changed: 0, indication: 0 },
+        THETA: { USD: 0, changed: 0, indication: 0 },
+        KMD: { USD: 0, changed: 0, indication: 0 },
+        PAX: { USD: 0, changed: 0, indication: 0 },
+        AE: { USD: 0, changed: 0, indication: 0 },
+        NPXS: { USD: 0, changed: 0, indication: 0 },
+        XVG: { USD: 0, changed: 0, indication: 0 },
+        SC: { USD: 0, changed: 0, indication: 0 },
+        BTM: { USD: 0, changed: 0, indication: 0 },
+        HT: { USD: 0, changed: 0, indication: 0 },
+        RVN: { USD: 0, changed: 0, indication: 0 },
+        DAI: { USD: 0, changed: 0, indication: 0 },
+        STRAT: { USD: 0, changed: 0, indication: 0 },
+        AOA: { USD: 0, changed: 0, indication: 0 },
+        IOST: { USD: 0, changed: 0, indication: 0 },
+        SNT: { USD: 0, changed: 0, indication: 0 },
+        PPT: { USD: 0, changed: 0, indication: 0 },
+        KCS: { USD: 0, changed: 0, indication: 0 },
+        GNT: { USD: 0, changed: 0, indication: 0 },
+        REPO: { USD: 0, changed: 0, indication: 0 },
+        PAI: { USD: 0, changed: 0, indication: 0 },
+        ARK: { USD: 0, changed: 0, indication: 0 },
+        ARDR: { USD: 0, changed: 0, indication: 0 },
+        R: { USD: 0, changed: 0, indication: 0 }
       }
     };
 
     this.tick = this.tick.bind(this);
-    this.determineStatus = this.determineStatus.bind(this);
   }
 
   componentDidMount() {
     this.tick();
-    window.setInterval(this.tick, 3000);
+    window.setInterval(this.tick, 100);
   }
 
   async tick() {
+    console.log("tick");
     const URL = `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${Object.keys(
       this.state.listings
     )}&tsyms=USD&api_key=${API_KEY}`;
@@ -107,27 +108,30 @@ class App extends Component {
         const key = keys[i];
         const previous = prevState.listings[keys[i]];
         const current = data[keys[i]];
-
         const changed = current.USD - previous.USD;
+        const indication = previous.indication;
+
+        if (changed < 0) {
+          if (indication == -1) {
+            indication = -2;
+          } else {
+            indication = -1;
+          }
+        } else if (changed > 0) {
+          if (indication == 1) {
+            indication = 2;
+          } else {
+            indication = 1;
+          }
+        }
         listings[keys[i]] = {
           USD: current.USD,
-          changed
+          changed,
+          indication
         };
       }
-
       return { listings };
     });
-  }
-
-  determineStatus(listings, oldListings, e, index) {
-    let updown = "";
-    if (e["USD"] > Object.values(oldListings)[index]["USD"]) {
-      updown = "up";
-    }
-    if (e["USD"] < Object.values(oldListings)[index]["USD"]) {
-      updown = "down";
-    }
-    return updown;
   }
 
   createTable(listings, min, max) {
@@ -139,17 +143,26 @@ class App extends Component {
       const key = keys[i];
       const listing = listings[key];
       let changedClass = "";
-      if (listing.changed > 0) {
-        changedClass = "up";
+      switch (listing.indication) {
+        case 1:
+          changedClass = " up";
+          break;
+        case 2:
+          changedClass = " up2";
+          break;
+        case -1:
+          changedClass = " down";
+          break;
+        case -2:
+          changedClass = " down2";
+          break;
+        default:
+          changedClass = "";
       }
-      if (listing.changed < 0) {
-        changedClass = "down";
-      }
-
       rows.push(
         <td>
           <div className="symbol">{Object.keys(listings)[i]}</div>
-          <div className={`price ${changedClass}`}>
+          <div className={`price${changedClass}`}>
             {Object.values(listings)[i]["USD"]}
           </div>
         </td>
@@ -162,6 +175,7 @@ class App extends Component {
 
   render() {
     const { listings } = this.state;
+
     return (
       <>
         <section className="overview">
